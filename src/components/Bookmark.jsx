a67cfbb7198a356e627ddb569707a6927f2e0f85
sagar parise <sagarparise01@gmail.com>
2024-02-05 11:08:10 +0530

    lastOne

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,18 +1,23 @@
 // components/Detail.js
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { IoMdShare } from "react-icons/io";
 import { CiHeart } from "react-icons/ci";
+import { FaGreaterThan } from "react-icons/fa6";
+import { FaRegArrowAltCircleLeft } from "react-icons/fa";
+
 const Bookmarks = () => {
   const { id } = useParams();
   const [text, setText] = useState(null);
   const [active, setActive] = useState('Details')
+  const navigate = useNavigate()
   useEffect(()=>{
     setText(post.body)
     setActive('Details')
   },[])
 
+ 
 
   const allPost = useSelector((state) => state.posts.posts);
 
@@ -23,6 +28,9 @@ console.log(allPost)
   if (!post) return <div>Loading...</div>;
 
   return (<>
+  <div className=' px-5 py-3 font-bold text-2xl flex justify-start items-center gap-4'>
+  <FaRegArrowAltCircleLeft onClick={()=> navigate('/')} /> {`Post Number ${post.id}`}
+  </div>
   <div class="flex max-w-2xl flex-col items-center rounded-md border p-3 md:flex-row m-5 ">
   <div class="h-full w-[50%] md:h-[200px] md:w-[300px] relative">
     <img
@@ -72,7 +80,7 @@ console.log(allPost)
 <div className='p-10 flex flex-wrap justify-center gap-3'>
 
 {allPost.map((post)=>(
-  <div key={post.id} className=" relative max-w-[350px] h-[350px] bg-white border p-4 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <div key={post.id} className=" relative max-w-sm h-[370px] w-[350px] bg-white border p-4 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
   <a href="#">
       <img className="rounded-t-lg w-full h-2/4" src={`https://picsum.photos/200?random=${post.id}`} alt="" />
   </a>
@@ -81,8 +89,18 @@ console.log(allPost)
           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white text-justify">{post.title.slice(0, 30)}</h5>
       </a>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">{`${post.body.slice(0, 70)}...`}</p>
-      
+      <span className="inline-flex items-center px-3 py-2 text-sm font-medium text-cent text-orange-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 absolute bottom-7 left-7">
+          Read more ....
+          
+      </span>
   </div>
+  <span className=' inline-block  bg-orange-500 p-2 rounded-lg text-white shadow-md font-bold absolute bottom-7 right-7' onClick={()=> {
+    setText(post.body)
+    setActive('Details')
+    navigate(`/Bookmarks/${post.id}`)
+  }} >
+<FaGreaterThan />
+</span>
 </div>
 ))}
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { CiSearch } from "react-icons/ci";
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
-
+import { FaGreaterThan } from "react-icons/fa6";
 function Home() {
   const posts = useSelector((state)=> state.posts.posts)
   const [searchQuery, setSearchQuery] = useState('');
@@ -33,13 +33,14 @@ function Home() {
               <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white text-justify">{post.title.slice(0, 30)}</h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">{`${post.body.slice(0, 70)}...`}</p>
-          <span onClick={()=> navigate(`/Bookmarks/${post.id}`)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 absolute bottom-7 left-7">
-              Read more
-               <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-              </svg>
+          <span className="inline-flex items-center px-3 py-2 text-sm font-medium text-cent text-orange-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 absolute bottom-7 left-7">
+              Read more ....
+              
           </span>
       </div>
+      <span className=' inline-block  bg-orange-500 p-2 rounded-lg text-white shadow-md font-bold absolute bottom-7 right-7' onClick={()=> navigate(`/Bookmarks/${post.id}`)} >
+<FaGreaterThan />
+</span>
   </div>
     ))}
 
